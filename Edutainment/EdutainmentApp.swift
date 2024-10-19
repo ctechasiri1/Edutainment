2600badc33bc9e9fc7ef81c869b749f03785bdbb
Chiraphat <ctechasiri@berkeley.edu>
2024-10-18 20:28:28 -0700

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Chiraphat Techasiri on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
